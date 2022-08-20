515bc623b1124731ec492053a4c01dfef033ffbc
nf-projects <nf-projects@users.noreply.github.com>
2022-08-20 08:22:45 -0700

    clean up code

--- src/App.js
+++ src/App.js
@@ -6,15 +6,6 @@ import SearchIcon from "./search.svg";
 
 const API_URL = "http://www.omdbapi.com?apikey=bfa26f5c";
 
-const movie1 = {
-  Title: "Superman IV: The Quest for Peace",
-  Year: "1987",
-  imdbID: "tt0094074",
-  Type: "movie",
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BMmIwZWY1YTYtNDlhOS00NDRmLWI4MzItNjk2NDc1N2NhYzNlXkEyXkFqcGdeQXVyNTUyMzE4Mzg@._V1_SX300.jpg",
-};
-
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
